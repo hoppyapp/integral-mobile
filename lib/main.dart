84d5567cca6d378f0cdeb13c7cc5ecc66549103c
David Gaspar <davidgaspar.dev@gmail.com>
2020-06-20 22:21:31 -0300

    Added fonts (Roboto)

--- lib/main.dart
+++ lib/main.dart
@@ -10,37 +10,36 @@ class Application extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      // This is the theme of your application.
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage()
+      home: HomePage(),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
-  
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
+      color: Theme.of(context).primaryColor,
       child: Center(
-        child: Text("Hello World!"),
+        child: Text(
+          "Hello World!",
+          style: TextStyle(
+            decoration: TextDecoration.none,
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.normal,
+            color: Colors.white
+          ),
+        ),
       ),
     );
   }
-  
 }
