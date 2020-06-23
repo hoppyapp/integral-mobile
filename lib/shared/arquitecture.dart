54d2b00e828c6511d563732af557393ec1f4e8f7
David Gaspar <davidgaspar.dev@gmail.com>
2020-06-23 15:00:05 -0300

    Arquitecture to BloC

--- lib/shared/arquitecture.dart
+++ lib/shared/arquitecture.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:integral_nutry/screens/login/constants.dart';
 
 abstract class Controller {
@@ -6,8 +7,16 @@ abstract class Controller {
 
 }
 
+abstract class LoginView {
+
+  Widget buildControl();
+
+}
+
 abstract class LoginControl implements Controller {
 
   Stream<VisibilityAction> get visibilityStream;
+
+  Widget loadControl();
 
 }
