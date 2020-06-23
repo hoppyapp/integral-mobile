33bbb62627ed76ef24b114e7cec4d50ddbac3fbc
David Gaspar <davidgaspar.dev@gmail.com>
2020-06-23 08:06:27 -0300

    Added flutter_svg package

---
+++ lib/screens/login/widgets/regiter_input.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginInputs extends AnimatedWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+}
