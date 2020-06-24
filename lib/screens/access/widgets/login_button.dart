db8ab1d08b8896ed32770ed36ef98b75d63e1bef
David Gaspar <davidgaspar.dev@gmail.com>
2020-06-24 15:41:30 -0300

    Adapted button design

--- lib/screens/access/widgets/login_button.dart
+++ lib/screens/access/widgets/login_button.dart
@@ -2,18 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:integral_nutry/screens/access/constants.dart';
 
+/// Login Button
+/// 
+/// At acccess screen to login with Google and Facebook
 class LoginButton extends StatelessWidget {
 
+  /// Attributes
   final Login login;
   final void Function() onLogin;
 
+  /// Constructor
   LoginButton(this.login, { this.onLogin });
 
   @override
+  /// Build widget
   Widget build(BuildContext context) {
 
+    final double width = MediaQuery.of(context).size.width * 0.75;
+    final double marginWidth =  (MediaQuery.of(context).size.width - width) / 2;
+    final double height = 40;
+
     String svgName;
-    double height = 40;
 
     switch(this.login) {
       case Login.google: 
@@ -27,10 +36,13 @@ class LoginButton extends StatelessWidget {
       default: Exception(this.login);
     }
 
+    // Return widget
     return Container(
       height: height,
+      width: width,
+      margin: EdgeInsets.symmetric(horizontal: marginWidth, vertical: 10),
       decoration: BoxDecoration(
-        color: Colors.white,
+        color: Colors.white.withOpacity(0.7),
         borderRadius: BorderRadius.circular(height/2),
         border: Border.all(color: Color(0xffc6c6c6), width: 1),
       ),
@@ -39,7 +51,7 @@ class LoginButton extends StatelessWidget {
         child: InkWell(
           onTap: this.onLogin,
           child: Center(
-            child: SvgPicture.asset("assets/images/svg/$svgName.svg", height: 25)
+            child: SvgPicture.asset("assets/images/svg/$svgName.svg", height: 20)
           ),
         ),
       ),
