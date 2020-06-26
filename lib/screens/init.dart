f19b30f95093334483953cff73515b800b259d2e
David Gaspar <davidgaspar.dev@gmail.com>
2020-06-26 10:40:53 -0300

    Implementing orientation setting

--- lib/screens/init.dart
+++ lib/screens/init.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:integral_nutry/screens/access/access_widget.dart';
+import 'package:integral_nutry/shared/system.dart';
 
 class Access extends StatefulWidget {
 
   /// Route name
   static final String route = "/access";
+
+  // Constrcutor
+  Access() {
+    // Config screen orientation
+    setSystemOrientation();
+  }
 
   @override
   /// Creating state
